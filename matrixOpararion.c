93400e22af7ce543f8ed53929ad5143342a97361
First-name Family-name <username@example.com>
2018-06-25 03:49:11 +0900

    行列名を指定して行列を入力する関数をつくった

--- matrixOpararion.c
+++ matrixOpararion.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
 
-int main(void){
+void makeMat(char name,int A[3][3]);
 
+
+int main(void){
     int A[3][3];
     int B[3][3]; 
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            printf("A[%d][%d]=",i+1,j+1);
-            scanf("%d",&A[i][j]);
-        }
-    printf("\n"); 
-    }
-    for(int i=0;i<3;i++){
-       for(int j=0;j<3;j++){
-           printf("B[%d][%d]=",i+1,j+1);
-           scanf("%d",&B[i][j]);
-       }
-    printf("\n"); 
-    }
-    
+
+    makeMat('A',A);
+    makeMat('B',B);
+
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             printf(" %d",A[i][j]);
@@ -39,3 +30,14 @@ int main(void){
 
     return 0;
 }
+
+void makeMat(char name,int mat[3][3]){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%c[%d][%d]=",name,i+1,j+1);
+            scanf("%d",&mat[i][j]);
+        }
+    printf("\n"); 
+    }
+}
+

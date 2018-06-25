def2f6dc08dc20507d329f782168fa432d0a192c
First-name Family-name <username@example.com>
2018-06-25 20:18:31 +0900

    行列の積を計算できるようにした

--- matrixOpararion.c
+++ matrixOpararion.c
@@ -2,8 +2,8 @@
 
 #define DEF_MAT(MAT) mkMat(#MAT,MAT)
 
+void printM(int mat[3][3]); 
 void mkMat(char name[], int A[3][3]);
-
 
 int main(void){
     int A[3][3];
@@ -12,25 +12,40 @@ int main(void){
     DEF_MAT(A);
     DEF_MAT(B);
 
+    printf("\nAxB=\n");
+
+    int outMat[3][3];
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            printf(" %d",A[i][j]);
+            outMat[i][j]=0;
         }
-        printf("\n");
+    }
+
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            for(int k=0;k<3;k++){ 
+                outMat[i][j] += A[i][j]*B[k][i];
+            }
+        }
+    printf("\n"); 
+    }
+
+    printM(outMat);
+
+    return 0;
+}
+
+void printM(int mat[3][3]){
+    for(int i=0;i<3;i++){
+        printf("[");
+        for(int j=0;j<3;j++){
+            printf(" %d",mat[i][j]);
+        }
+        printf(" ]\n");
     }
     
     printf("\n");
-
-    for(int i=0;i<3;i++){
-        for(int j=0;j<3;j++){
-            printf(" %d",B[i][j]);
-        }
-        printf("\n");
-    }
-
-
-
-    return 0;
 }
 
 void mkMat(char name[],int mat[3][3]){
@@ -42,5 +57,5 @@ void mkMat(char name[],int mat[3][3]){
         }
     printf("\n"); 
     }
+    printM(mat);
 }
-

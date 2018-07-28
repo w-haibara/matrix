c1d9158597331ec536e6d63862c6a175642065a4
First-name Family-name <username@example.com>
2018-07-28 10:40:37 +0900

    printの文言を変更

---
+++ rowReduction.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+#define DEF_MAT(MAT) mkMat(#MAT,MAT)
+
+void printMat(int mat[3][3]); 
+void mkMat(char name[], int A[3][3]);
+
+int main(void){
+    int A[3][3];
+    int B[3][3]; 
+
+    DEF_MAT(A);
+    DEF_MAT(B);
+
+    printf("\nAB=\n");
+
+    int outMat[3][3];
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            outMat[i][j]=0;
+        }
+    }
+
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            for(int k=0;k<3;k++){ 
+                outMat[i][j] += A[i][j]*B[k][i];
+            }
+        }
+    printf("\n"); 
+    }
+
+    printMat(outMat);
+
+    return 0;
+}
+
+void printMat(int mat[3][3]){
+    for(int i=0;i<3;i++){
+        printf("[");
+        for(int j=0;j<3;j++){
+            printf(" %d",mat[i][j]);
+        }
+        printf(" ]\n");
+    }
+    
+    printf("\n");
+}
+
+void mkMat(char name[],int mat[3][3]){
+
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            printf("%s[%d][%d]=",name ,i+1,j+1);
+            scanf("%d",&mat[i][j]);
+        }
+    printf("\n"); 
+    }
+    printMat(mat);
+}
